0773ec575bfb32b7ddeaaeeb9ab83b6b40ec3fa3
Andrey Zakharov <mediaexplorer74@hotmail.com>
2022-02-27 19:12:32 +0300

    .

---
+++ Win3Mu/ConFrames/ConsoleKeyInfo.cs
@@ -0,0 +1,13 @@
+﻿/*
+ConFrames - Gui Stuff for Console Windows
+Copyright (C) 2017-2018 Topten Software.
+*/
+
+namespace ConFrames
+{
+    //
+    public class ConsoleKeyInfo
+    {
+        //
+    }
+}
